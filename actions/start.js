8b40392dafd7ba3c1dbee3cc83433e71de3cbdd4
stormy482 <storm1surge@gmail.com>
2021-09-12 00:45:32 -0700

    [Pat] fixed a fuckey wuckey. outlined component

---
+++ actions/start.js
@@ -0,0 +1,14 @@
+// joins component to vc begins listening process
+const { VoiceChannel } = require("discord.js");
+
+module.exports = {
+    action: "start",
+    /**
+     * 
+     * @param {VoiceChannel} channel 
+     */
+    async start(channel) {
+        fetch()
+    }
+
+};
